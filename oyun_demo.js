cdac4b598311befa124aa8db9cb7b084ec019507
Emirhan019 <cemirhan222@gmail.com>
2026-09-14 22:04:06 +0300

    game works score and game over screen is not ready

--- oyun_demo.js
+++ oyun_demo.js
@@ -1,6 +1,25 @@
 //let spawn_position_x = Math.floor(Math.random() * 10) + 800;
 //let spawn_position_y = Math.floor(Math.random() * 10) + 600;
+let attack_image_left = new Image();
+let attack_image_up = new Image();
+let attack_image_down = new Image();
+let attack_image = new Image();
+let attack_dx = 0;
+let attack_dy = 0;
+let attack_distance = 0;
+attack_image_left.src = "oyunresim/atatck_l.png";
+attack_image_down.src = "oyunresim/attack_down.png";
+attack_image_up.src = "oyunresim/attack_up.png";
+attack_image.src = "oyunresim/attack.png";
+let facing = "";
+let last_attack = 0;
+let attack_speed = 500;
+let attack_img_speed = 200;
 let enemies = [];
+let attack_x = 0;
+let attack_y = 0;
+let space_pressed = false;
+let i_frame_text = document.getElementById("i_frame_info");
 let hp_text = document.getElementById("hp");
 let enemie_speed = 1;
 let last_damaged = 0;
@@ -16,31 +35,32 @@ let player_image = new Image();
 let enemie_image = new Image();
 enemie_image.src = "oyunresim/demoenimie.png"
 player_image.src = "oyunresim/demoplayer.png"
-function test_draw(){
-    ctx.beginPath();
-    ctx.drawImage(player_image, zero_x, zero_y, 36, 44);
-    ctx.closePath();
-    requestAnimationFrame(test_draw)
-}
+
 
 
 function get_player_position(){
     if (keys.w) {
+        facing = "up";
          if (zero_y > 0)
-            zero_y -= player_speed
+            zero_y -= player_speed;
+
     }
         if (keys.a) {
+            facing = "left";
             if (zero_x > 0)
-            zero_x -= player_speed
+            zero_x -= player_speed;
     }
         if (keys.s) {
+            facing = "down";
         if (zero_y + 40 < canvas.height)
-            zero_y += player_speed
+            zero_y += player_speed;
     }
         if (keys.d) {
+            facing = "right";
             if (zero_x + 50 < canvas.width)
-                zero_x += player_speed
+                zero_x += player_speed;
     }
+
 }
 function enemie_location_updater(){
     enemies.forEach((enemie) => {
@@ -69,12 +89,7 @@ setInterval(() => {
 
 }, 5000)
 
-    document.addEventListener("keydown" , function(e){
-    keys[e.key] = true;
-    });
-    document.addEventListener("keyup" , function(e){
-    keys[e.key] = false;
-    });
+
 
 function closebutton(){
     button = document.getElementById("button")
@@ -82,27 +97,106 @@ function closebutton(){
 }
 
 
+    document.addEventListener("keydown" , function(e){
+    keys[e.key] = true;
+    if (e.key === " " && Date.now() - last_attack > attack_speed){
+        last_attack = Date.now()
+        space_pressed = true;
+        console.log("keydown_space")
+    }
+    });
+
+
+    document.addEventListener("keyup" , function(e){
+    keys[e.key] = false;
+    if (e.key === " "){
+        space_pressed = false;
+        console.log("keyup_space")
+    }
+    });
+
+
+function attack_image_draw(){
+    if ( Date.now() - last_attack < attack_img_speed) {
+     switch (facing) {
+        case "right":
+            ctx.drawImage(attack_image, attack_x, attack_y, 39, 44 );
+            break;
+     
+        case "left":
+            ctx.drawImage(attack_image_left, attack_x, attack_y, 39, 44);
+            break;
+
+        case "up":
+            ctx.drawImage(attack_image_up, attack_x, attack_y, 39, 44 );
+            break;
+
+        case "down":
+            ctx.drawImage(attack_image_down, attack_x, attack_y, 39, 44 );
+            break;
+     }   
+        
+    }
+
+}
+
+function attack_check(){
+    switch (facing) {
+        case "right": attack_x = zero_x + 40; attack_y = zero_y; break;
+        case "left": attack_x = zero_x - 50; attack_y = zero_y; break;
+        case "up": attack_x = zero_x; attack_y = zero_y - 50; break;
+        case "down": attack_x = zero_x; attack_y = zero_y + 50; break;
+    }
+
+
+}
+
+
+
+
 function gameLoop(){
     
 
     ctx.beginPath();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     get_player_position();
+    attack_check();
+    attack_image_draw();
+
+
+
+
+
+
+
     enemie_location_updater();
-    for (let enemie of enemies) {
+    for (let i = enemies.length -1 ;i >= 0; i--) {
+    let enemie = enemies[i];
     ctx.drawImage(enemie_image, enemie.x, enemie.y, 30,30)
     let dx = zero_x - enemie.x;
     let dy = zero_y - enemie.y;
     let distance = Math.sqrt(dx*dx + dy*dy);
-    if(distance < 40 && Date.now() - last_damaged > i_frame){
-        last_damaged = Date.now()
-        player_hp -= 1
-        console.log("touched")
+    if(distance < 38 && Date.now() - last_damaged > i_frame){
+        last_damaged = Date.now();
+        player_hp -= 1;
+        console.log("touched");
+        i_frame_text.textContent = "I_FRAME_ACTIVE";
     }
+    attack_dx = attack_x - enemie.x;
+    attack_dy = attack_y - enemie.y;
+    attack_distance = Math.sqrt(attack_dx*attack_dx + attack_dy*attack_dy);
+    if (attack_distance <= 50 && Date.now() - last_attack < attack_img_speed) {
+        enemies.splice(i, 1);
+        continue;
     }
-    
-    ctx.drawImage(player_image, zero_x, zero_y, 50, 40);
+
+
+    }
+    if (Date.now() - last_damaged > i_frame) {
+        i_frame_text.textContent = "";
+    }
+
+    ctx.drawImage(player_image, zero_x, zero_y, 40, 40);
     ctx.closePath();
     hp_text.textContent = player_hp;
     requestAnimationFrame(gameLoop)
